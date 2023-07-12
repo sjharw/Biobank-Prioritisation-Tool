4bc52f2ce965f77d63249a4fdde664bba052c787
Sarah Jane Harwood <sarahjharwood@googlemail.com>
2023-07-12 09:44:38 +0100

    success alert added for download

--- app.py
+++ app.py
@@ -202,6 +202,7 @@ def display():
     # download data if download button clicked
     if "download" in request.form:
         data.to_csv(os.path.join(DOWNLOAD_DIR + "priority-scores.csv"))
+        flash(f"Dataset downloaded to '/downloaded' folder")
 
     # get class information for filters
     classes = list(data["class"].drop_duplicates())
